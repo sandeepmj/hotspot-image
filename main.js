939a0b3adfbadac69d4a98211934b62f5ca8a9cb
sskerr <sarah.s.kerr@gmail.com>
2017-03-23 13:20:34 -0400

    Remove test alert

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-alert("Hello, world");
-
 $(document).ready(function () { // we must wait for the DOM to be ready as the browser can take a little while to read all of the HTML and build the elements
 
 	$(".circle").on("click", function (e) {
